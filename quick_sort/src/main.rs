194f5a5677b822fbbc8cd6190921049389d9fc5b
JakubDziegielewski <kubadziegiel1@gmail.com>
2023-02-11 10:13:02 +0100

    cosmetic changes

--- quick_sort/src/main.rs
+++ quick_sort/src/main.rs
@@ -12,23 +12,24 @@ fn main() {
 }
 
 fn quick_sort(mut a: &mut [i32], l: i32, r: i32) {
+    //clasic method
     if l >= r {
         return;
     }
 
-    let mut index = l;
+    let mut j = l;
 
     for i in l + 1..r + 1 {
         if a[i as usize] <= a[l as usize] {
-            index = index + 1;
-            (a[i as usize], a[index as usize]) = (a[index as usize], a[i as usize]);
+            j = j + 1;
+            (a[i as usize], a[j as usize]) = (a[j as usize], a[i as usize]);
         }
     }
 
-    (a[l as usize], a[index as usize]) = (a[index as usize], a[l as usize]);
+    (a[l as usize], a[j as usize]) = (a[j as usize], a[l as usize]);
 
-    quick_sort(&mut a, l, index - 1);
-    quick_sort(&mut a, index + 1, r);
+    quick_sort(&mut a, l, j - 1);
+    quick_sort(&mut a, j + 1, r);
 }
 fn quick_sort_with_shorter_slicec(a: &mut [i32], l: i32, r: i32) {
     if l >= r {
@@ -36,22 +37,22 @@ fn quick_sort_with_shorter_slicec(a: &mut [i32], l: i32, r: i32) {
     }
     //recursive methods are only given the slice, that they are supposed to sort
 
-    let mut iter = 0;
+    let mut j = 0;
     for i in 1..(r - l + 1) {
         if a[i as usize] <= a[0] {
-            iter = iter + 1;
-            (a[i as usize], a[iter as usize]) = (a[iter as usize], a[i as usize]);
+            j = j + 1;
+            (a[i as usize], a[j as usize]) = (a[j as usize], a[i as usize]);
         }
     }
 
-    (a[0], a[iter as usize]) = (a[iter as usize], a[0]);
+    (a[0], a[j as usize]) = (a[j as usize], a[0]);
 
-    if (iter - 1) > 0 {
-        quick_sort_with_shorter_slicec(&mut a[0..iter as usize], 0, iter - 1);
+    if (j - 1) > 0 {
+        quick_sort_with_shorter_slicec(&mut a[0..j as usize], 0, j - 1);
     }
     let len = a.len();
-    if (len - 1) as i32 > (iter + 1) {
-        quick_sort_with_shorter_slicec(&mut a[iter as usize + 1..len], iter + 1, len as i32 - 1);
+    if (len - 1) as i32 > (j + 1) {
+        quick_sort_with_shorter_slicec(&mut a[j as usize + 1..len], j + 1, len as i32 - 1);
     }
 }
 fn _quick_sort_with_new_vector(a: &[i32], l: i32, r: i32) -> Vec<i32> {
