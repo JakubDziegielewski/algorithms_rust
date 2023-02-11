2e60fc7462eb24a9787823fd02b62a8dacf9d599
JakubDziegielewski <kubadziegiel1@gmail.com>
2023-02-11 10:50:33 +0100

    code improved

--- quick_sort/src/main.rs
+++ quick_sort/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let mut a: &mut [i32] = &mut [0, 1, -2, 10, 11, 0, -10, 92, 2110, 1, -73, 222, 120, 920, 0];
+    let mut a: &mut [i32] = &mut [1, 2, 4, 3, 1, 1, 8, -1];
 
     let len = a.len();
     let mut b: Vec<i32> = vec![0; len];
     b.clone_from_slice(&a[..]);
     quick_sort(&mut a, 0, len as i32 - 1);
-    quick_sort_with_shorter_slicec(&mut b, 0, len as i32 - 1);
+    quick_sort_with_shorter_slicec(&mut b);
     assert_eq!(a, b);
 
     println!("{:?}", a)
@@ -31,29 +31,23 @@ fn quick_sort(mut a: &mut [i32], l: i32, r: i32) {
     quick_sort(&mut a, l, j - 1);
     quick_sort(&mut a, j + 1, r);
 }
-fn quick_sort_with_shorter_slicec(a: &mut [i32], l: i32, r: i32) {
-    if l >= r {
+fn quick_sort_with_shorter_slicec(a: &mut [i32]) {
+    if a.len() <= 0 {
         return;
     }
     //recursive methods are only given the slice, that they are supposed to sort
-
+    let len = a.len();
     let mut j = 0;
-    for i in 1..(r - l + 1) {
+    for i in 1..len {
         if a[i as usize] <= a[0] {
             j = j + 1;
             (a[i as usize], a[j as usize]) = (a[j as usize], a[i as usize]);
         }
     }
-
     (a[0], a[j as usize]) = (a[j as usize], a[0]);
 
-    if (j - 1) > 0 {
-        quick_sort_with_shorter_slicec(&mut a[0..j as usize], 0, j - 1);
-    }
-    let len = a.len();
-    if (len - 1) as i32 > (j + 1) {
-        quick_sort_with_shorter_slicec(&mut a[j as usize + 1..len], j + 1, len as i32 - 1);
-    }
+    quick_sort_with_shorter_slicec(&mut a[0..j as usize]);
+    quick_sort_with_shorter_slicec(&mut a[j as usize + 1..len]);
 }
 fn _quick_sort_with_new_vector(a: &[i32], l: i32, r: i32) -> Vec<i32> {
     let mut sorted = Vec::from(a);
